5cfe384321aac1dd102518f22598136ccc857af4
sahan.senvar <sahan.senvar@partners.getir.com>
2024-05-06 01:36:09 +0300

    Revert "initial commit"
    This reverts commit 1efbdbea2018f66be7bab58be9c03ecff08edce5.

--- feature/auth/data/build.gradle.kts
+++ feature/auth/data/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 kotlin {
     cocoapods {
-        name = path.replace(':', '.').drop(1)
+        name = "authData"
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
         version = "1.0"
@@ -15,7 +15,7 @@ kotlin {
         ios.deploymentTarget = "16.0"
         podfile = rootProject.file("iosApp/Podfile")
         framework {
-            baseName = path.replace(':', '.').drop(1)
+            baseName = "authData"
             isStatic = true
         }
     }
