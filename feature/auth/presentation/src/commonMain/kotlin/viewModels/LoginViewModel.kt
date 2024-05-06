8ba35e0cb902aa78c4f26fa913018667057c33c1
sahan.senvar <sahan.senvar@partners.getir.com>
2024-05-06 18:16:58 +0300

    LoginViewModel has created as empty

--- feature/auth/presentation/src/commonMain/kotlin/viewModels/LoginViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/viewModels/LoginViewModel.kt
@@ -1,6 +1,15 @@
 package viewModels
 
-class LoginViewModel(
+import useCases.GetBackgroundImageUseCase
+import useCases.TryLoginUseCase
+import useCases.ValidateIdUseCase
+import useCases.ValidatePasswordUseCase
 
+class LoginViewModel(
+    private val getBackgroundImageUseCase: GetBackgroundImageUseCase,
+    private val tryLoginUseCase: TryLoginUseCase,
+    private val validateIdUseCase: ValidateIdUseCase,
+    private val validatePasswordUseCase: ValidatePasswordUseCase
 ){
+
 }
