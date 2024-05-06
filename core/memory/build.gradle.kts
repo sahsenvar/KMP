467ab31a74cc31e9a83b9b8746b0171032c0f7f1
sahan.senvar <sahan.senvar@partners.getir.com>
2024-05-06 18:15:17 +0300

    memory module has created with CacheService(empty) and StoreService(empty)

---
+++ core/memory/build.gradle.kts
@@ -0,0 +1,32 @@
+apply(from = rootProject.file("base-gradle.gradle"))
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinCocoapods)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0"
+        group = getNameSpace(project = project)
+        ios.deploymentTarget = "16.0"
+        podfile = rootProject.file("iosApp/Podfile")
+        framework {
+            baseName = "memory"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+
+        }
+        commonTest.dependencies {
+
+        }
+    }
+}
+
+android.namespace = getNameSpace(project = project)
